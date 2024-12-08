5bda0e673837bf28e9d2815febfedb8dc4ad2a75
Mr-Keks <levelel744@gmail.com>
2024-12-08 16:53:02 +0100

    replace tkinter with pyperclip

--- kb.py
+++ kb.py
@@ -7,8 +7,7 @@ import time
 import pyperclip
 
 
-last_users_buffer_value = None
-
+LAST_USER_BUFFER_TEXT_VALUE = None
 
 def load_languages():
     # Load language mappings from a JSON file
@@ -31,35 +30,27 @@ def change_text(coppied_text, mapping):
     return correct
 
 def copy_text():
-    global last_users_buffer_value
-    last_users_buffer_value = pyperclip.paste()
+    global LAST_USER_BUFFER_TEXT_VALUE
+    LAST_USER_BUFFER_TEXT_VALUE = pyperclip.paste()
     time.sleep(0.2)
     pyautogui.hotkey('ctrl', 'a')
     pyautogui.hotkey('ctrl', 'c')
     time.sleep(0.2)  # Increase the sleep time to ensure the clipboard updates
 
 def take_a_text():
-    root = tk.Tk()
-    root.withdraw()  # Hide the main window
-    clipboard_content = ""
     try:
-        clipboard_content = root.clipboard_get().strip()
-    except tk.TclError:
-        print("Failed to get clipboard content")
-    return clipboard_content
+        return pyperclip.paste()
+    except Exception as ex:
+        print("take a text exeption: ", ex)
 
 def replace_text(correct_text):
     pyautogui.hotkey('ctrl', 'a')
-    pyautogui.hotkey('delete')  # Clear the text area
-    time.sleep(0.2)  # Ensure the text area is cleared
-    # pyautogui.typewrite(correct_text, interval=0.01)
+    pyautogui.hotkey('delete') 
+    time.sleep(0.2)  
     keyboard.write(correct_text)
-    time.sleep(0.5) 
-    input("...")
-    global last_users_buffer_value
-    pyperclip.copy(last_users_buffer_value)
-    # r = pyperclip.paste()
-    # keyboard.write(r)
+    time.sleep(0.1) 
+    global LAST_USER_BUFFER_TEXT_VALUE
+    pyperclip.copy(LAST_USER_BUFFER_TEXT_VALUE)
 
 def execute_all():
     copy_text()
